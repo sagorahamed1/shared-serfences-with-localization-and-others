fc3c7167f47675310f33565c838735d20023aa30
sagor ahamed <sagorahammed002@gmail.com>
2023-11-07 17:17:30 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/localization_language_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.localization_language_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late Locale _appLocale;
+
+  //get locale languege from ElevatedButton as a parameter
+  Future <void> _changeLanguage(Locale locale) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+     prefs.setString('language', locale.languageCode);
+    setState(() {
+      _appLocale = locale;
+    });
+  }
+
+
+  Future <void> _loadLocale() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String languageCode = prefs.getString('language') ?? 'en';
+    setState(() {
+      _appLocale = Locale(languageCode);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+
+      localizationsDelegates: [
+         AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
+
+      locale: _appLocale,
+      supportedLocales: [
+        Locale('en'),
+        Locale('bn'),
+      ],
+      home: HomePage(changeLanguage: _changeLanguage),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final Function changeLanguage;
+
+  const HomePage({Key? key, required this.changeLanguage});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context)!.appbar)),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          SizedBox(height: 60,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // language == বাংলা ? locale er maje enlish asbe and na hoile locale er maje bangla asbe
+                   var locale = AppLocalizations.of(context)!.language == 'বাংলা' ? Locale('bn') : Locale('en');
+                   // when we press this buttom. call _changeLaguage method and change the locale language
+                   widget.changeLanguage(locale);
+                },
+                child: Container(child: Text(AppLocalizations.of(context)!.language))
+              ),
+            ],
+          ),
+
+           Text(AppLocalizations.of(context)!.bangladesh,style: TextStyle(fontSize: 30),),
+
+           const SizedBox(height: 30,),
+
+           Text(AppLocalizations.of(context)!.bangladesh_history)
+
+        ],
+      ),
+    );
+  }
+}
